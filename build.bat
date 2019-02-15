9e6b761ea9c468842126bbea4bbcd00e89ca4b95
Normmatt <normmatt234@gmail.com>
2019-02-15 15:01:08 +1300

    Dump another chunk of text.

--- build.bat
+++ build.bat
@@ -2,4 +2,5 @@ copy rom\input.gba rom\output.gba
 armips.exe asm/VWF.asm
 Atlas rom\output.gba script\stafy_script_000.txt
 Atlas rom\output.gba script\stafy_script_001.txt
+Atlas rom\output.gba script\stafy_script_002.txt
 pause
